a55149e4396b91ebde71a4f99fecdc9f93b0d3d8
Manavjit Singh <80977693+Kuruin@users.noreply.github.com>
2026-04-21 06:25:58 +0530

    Update Prac5.c

--- c-programs/Prac5.c
+++ c-programs/Prac5.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() { char exp[20];
-int i, temp = 1; printf("Enter expression: "); scanf("%s", exp);
-printf("\nThree Address Code:\n"); for(i = 0; exp[i] != '\0'; i++) {
-if(exp[i] == '+' || exp[i] == '-' || exp[i] == '*' || exp[i] == '/') {
-printf("t%d = %c %c %c\n", temp, exp[i-1], exp[i], exp[i+1]); exp[i+1] = 't'; // replace with temp (simplified)
-temp++;
-}
-}
+int main() {
+    char exp[20];
+    int i, temp = 1;
 
-printf("Result stored in t%d\n", temp-1);
+    printf("Enter expression: ");
+    scanf("%s", exp);
 
-return 0;
+    printf("\nThree Address Code:\n");
+
+    for (i = 0; exp[i] != '\0'; i++) {
+        if (exp[i] == '+' || exp[i] == '-' || exp[i] == '*' || exp[i] == '/') {
+            printf("t%d = %c %c %c\n", temp, exp[i - 1], exp[i], exp[i + 1]);
+            exp[i + 1] = 't'; // replace with temp (simplified)
+            temp++;
+        }
+    }
+
+    printf("Result stored in t%d\n", temp - 1);
+
+    return 0;
 }
