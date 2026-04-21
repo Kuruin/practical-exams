d9a8620e556da4c184d57157b98a8a661fd0d8fb
Manavjit Singh <80977693+Kuruin@users.noreply.github.com>
2026-04-21 06:25:35 +0530

    Update Prac4.c

--- c-programs/Prac4.c
+++ c-programs/Prac4.c
@@ -1,26 +1,53 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-    char input[20];
-    int i;
+char s[20] = "$E", in[20], *p;
+int t = 1, i = 0;
 
-    printf("Enter input: ");
-    scanf("%s", input);
-
-    // Check pattern: i+i+i...
-    if (input[0] != 'i') {
-        printf("Rejected\n");
-        return 0;
+void push(char *m) {
+    for (int l = strlen(m) - 1; l >= 0; l--) {
+        s[++t] = m[l];
     }
+}
 
-    for (i = 1; input[i] != '\0'; i += 2) {
-        if (input[i] != '+' || input[i + 1] != 'i') {
-            printf("Rejected\n");
-            return 0;
+int main() {
+    printf("Enter input: ");
+    scanf("%s", in);
+
+    strcat(in, "$");
+
+    printf("\nStack\tInput\tAction\n");
+
+    while (t >= 0) {
+        printf("%s\t%s\t", s, in + i);
+
+        char c = s[t--], c_in = in[i];
+        s[t + 1] = 0;
+
+        if (c == c_in) {
+            printf("match\n");
+            i++;
+        }
+        else if (c == 'E') {
+            printf("E->TE'\n");
+            push("TX");
+        }
+        else if (c == 'X') {
+            if (c_in == '+') {
+                printf("E'->+TE'\n");
+                push("+TX");
+            } else {
+                printf("E'->ε\n");
+            }
+        }
+        else if (c == 'T' && c_in == 'i') {
+            printf("T->i\n");
+            push("i");
+        }
+        else {
+            return printf("Rejected\n"), 0;
         }
     }
 
-    printf("Accepted\n");
-    return 0;
+    printf(in[i] == 0 ? "Accepted\n" : "Rejected\n");
 }
